894a475f805fdf07c2abf4fa15c763c58a33926e
Sackitey68 <sackitey68@gmail.com>
2023-12-20 09:47:54 +0000

    Added the time stamp

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -1,11 +1,18 @@
 export default class NotesAPI {
     static getAllNotes() {
         const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
-        return notes
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+        });
     }
 
     static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
 
+        noteToSave.id = Math.floor(Math.random() * 1000000);
+        noteToSave.updated = new Date().toISOString();
+        notes.push(noteToSave);
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
 
     static deleteNote(id) {
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import NotesAPI from  "./NotesAPI.js"
 
+NotesAPI.saveNote({
+    title: "New Notes!",
+    body: "I am a new notes."
+});
+
 console.log(NotesAPI.getAllNotes());
